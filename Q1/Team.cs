892136f3f5dcbcf782d0918597921df52bb3f54f
s00185883 <s00185883@LAB-B1041-18>
2019-10-08 12:27:37 +0100

    Add project files.

---
+++ Q1/Team.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    class Team
+    {
+        public string TeamName { get; private set; }
+        public int TeamWins { get; private set; }
+        public int TeamLosses { get; private set; }
+        public int TeamDraw { get; private set; }
+        public int TeamPlayed { get; private set; }
+        private int _points;
+        public int Points
+        {
+            get
+            {
+                _points = (TeamWins * 3) + (TeamDraw * 1);
+                return _points;
+            }    
+        }
+
+    }
+}
